1439025d3f7576458413914411e4db760aaf2307
lkxiaojian <18513075404@163.com>
2018-07-03 14:44:03 +0800

    userinfo

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -1,4 +1,6 @@
 // pages/mine/mine.js
+const app = getApp()
+
 Component({
   /**
    * 组件的属性列表
@@ -18,6 +20,80 @@ Component({
    * 组件的方法列表
    */
   methods: {
+    onLoad: function () {
+wx.login({
+  success:function(res){
+    console.log(res.code)
+
+    wx.request({
+      url: 'https://api.weixin.qq.com/sns/jscode2session',
+      header: {
+        'content-type': 'application/json'
+      },
+      data:{
+        appid:"wx6edb7f61313145d2",
+        secret:"f33ac85ef10f33617382660d19eb73d1",
+        js_code:res.code,
+        grant_type:"authorization_code"
+      },
+      success:function(r){
+        
+        console.log(r)
+      }
+
+      
+    })
+
+  }
+});
+
+
+
+
+      if (app.globalData.userInfo) {
+        this.setData({
+          userInfo: app.globalData.userInfo,
+          hasUserInfo: true
+        })
+      } else if (this.data.canIUse) {
+        // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+        // 所以此处加入 callback 以防止这种情况
+        app.userInfoReadyCallback = res => {
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      } else {
+        // 在没有 open-type=getUserInfo 版本的兼容处理
+        wx.getUserInfo({
+          success: res => {
+            app.globalData.userInfo = res.userInfo
+            this.setData({
+              userInfo: res.userInfo,
+              hasUserInfo: true
+            })
+          }
+        })
+      }
+    },
+    getUserInfo: function (e) {
+      console.log(e)
+      app.globalData.userInfo = e.detail.userInfo
+      this.setData({
+        userInfo: e.detail.userInfo,
+        hasUserInfo: true
+      })
+    },
+    getPhoneNumber:function(e){
+      console.log(e.detail.errMsg)
+      console.log(e.detail.iv)
+      console.log(e.detail.encryptedData) 
+    },
+
+
+
+
 
   }
 })
